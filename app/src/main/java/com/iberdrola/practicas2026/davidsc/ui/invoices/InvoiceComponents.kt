c6208d144bae9e8ae3e6b0093909aa34582a489b
davidsolero <100933277+davidsolero@users.noreply.github.com>
2026-03-26 13:10:45 +0100

    UI: padding systems

--- app/src/main/java/com/iberdrola/practicas2026/davidsc/ui/invoices/InvoiceComponents.kt
+++ app/src/main/java/com/iberdrola/practicas2026/davidsc/ui/invoices/InvoiceComponents.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -239,7 +242,12 @@ fun InvoicesHeader(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 12.dp)
+            .padding(
+                top =  WindowInsets.statusBars.asPaddingValues().calculateTopPadding(),
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 12.dp
+            )
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically
