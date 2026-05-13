eb556cb9870ac8fb9d96f0bb986483190cef7175
davidsolero <100933277+davidsolero@users.noreply.github.com>
2026-05-13 10:24:49 +0200

    UI: FE focus

--- app/src/main/java/com/iberdrola/practicas2026/davidsc/ui/contract/ContractSelectionScreen.kt
+++ app/src/main/java/com/iberdrola/practicas2026/davidsc/ui/contract/ContractSelectionScreen.kt
@@ -2,15 +2,7 @@ package com.iberdrola.practicas2026.davidsc.ui.contract
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,17 +10,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
 import androidx.compose.material.icons.outlined.Lightbulb
 import androidx.compose.material.icons.outlined.Whatshot
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,13 +21,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import com.iberdrola.practicas2026.davidsc.R
-import com.iberdrola.practicas2026.davidsc.ui.navigation.Screen
 import com.iberdrola.practicas2026.davidsc.domain.model.Contract
 import com.iberdrola.practicas2026.davidsc.domain.model.ContractType
 import com.iberdrola.practicas2026.davidsc.ui.invoices.BackButton
 import com.iberdrola.practicas2026.davidsc.ui.navigation.SafeNavController
+import com.iberdrola.practicas2026.davidsc.ui.navigation.Screen
 
 @Composable
 fun ContractSelectionScreen(
@@ -55,12 +37,13 @@ fun ContractSelectionScreen(
     var isExiting by remember { mutableStateOf(false) }
 
     Scaffold { innerPadding ->
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(horizontal = dimensionResource(R.dimen.margin_medium))
         ) {
+
             Spacer(modifier = Modifier.height(dimensionResource(R.dimen.margin_medium)))
 
             BackButton(onClick = {
@@ -75,13 +58,17 @@ fun ContractSelectionScreen(
             Text(
                 text = stringResource(R.string.contract_selection_title),
                 style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.margin_medium))
             )
 
             Spacer(modifier = Modifier.height(dimensionResource(R.dimen.margin_large)))
 
-            LazyColumn {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
                 items(contracts) { contract ->
+
                     ContractItem(
                         contract = contract,
                         onClick = {
@@ -96,12 +83,14 @@ fun ContractSelectionScreen(
                             }
                         }
                     )
+
                     HorizontalDivider(color = Color.LightGray)
                 }
             }
         }
     }
 }
+
 @Composable
 private fun ContractItem(
     contract: Contract,
